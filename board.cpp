c7f6843e4e90cad390bb64e519eaa186f0ea10a5
Mohymin Islam <islammohymin@gmail.com>
2026-05-08 23:46:59 -0400

    some more work

--- board.cpp
+++ board.cpp
@@ -1,7 +1,8 @@
 #include <vector>
 #include <cmath>
 #include <iostream>
-#include<piece.cpp>
+#include <piece.cpp>
+#include <cstdint>
 
 using std::cout;
 using std::endl;
@@ -11,7 +12,7 @@ using std::vector;
 
 class Board {
 private:
-    unint64_t emptyBoard = 0x0000000000000000; // for future reference
+    uint64_t emptyBoard = 0x0000000000000000; // for future reference
     // every two digits represents a rank in the board
     // 00FF000000000000 -> empty squares on the first rank
     // Second rank entirely filled
@@ -19,11 +20,28 @@ private:
 
     // We can hardcode bitboards since there will always be 12 for the pieces
     // and several more for calculations
-
-    uint64_t allPieces = 0xFFFF00000000FFFF; // All pieces - first and last 2 rows occupied
+    
+    // All pieces - first and last 2 rows occupied
+    uint64_t allPieces = 0xFFFF00000000FFFF; 
     uint64_t whitePieces = 0xFFFF000000000000;
     uint64_t blackPieces = 0xFFFF000000000000;
     uint64_t enPassant = 0x0000000000000000;
+
+    // white pieces
+    uint64_t whitePawns = 0x0000000000000000;
+    uint64_t whiteRooks = 0x0000000000000000;
+    uint64_t whiteKnights = 0x0000000000000000;
+    uint64_t whiteBishops = 0x0000000000000000;
+    uint64_t whiteQueens = 0x0000000000000000;
+    uint64_t whiteKing = 0x0000000000000000; 
+
+    // black pieces
+    uint64_t blackPawns = 0x0000000000000000;
+    uint64_t blackRooks = 0x0000000000000000;
+    uint64_t blackKnights = 0x0000000000000000;
+    uint64_t blackBishops = 0x0000000000000000;
+    uint64_t blackQueens = 0x0000000000000000;
+    uint64_t blackKing = 0x0000000000000000; 
 
 
 
@@ -31,5 +49,11 @@ public:
     Board() {
 
     } // default constructor
+
+    void print() {
+
+    }
+
+
 
 };
